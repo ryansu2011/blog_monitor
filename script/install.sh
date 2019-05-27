8d217eb1a85fbbdfaf1f3716539454a57c55dd43
Yiran Su <ryansu2011@gmail.com>
2019-05-27 14:55:34 -0400

    CentOS prerequisite installation

---
+++ script/install.sh
@@ -0,0 +1,8 @@
+# centos
+sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum update -y
+sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
+sudo yum install git-core
+sudo yum install gcc
+pip3.6 install requests
+pip3.6 install Scrapy
